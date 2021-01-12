795d841495c0b3a8a376f3914cf2193a48ea7465
Adarsh1231 <74771559+Adarsh1231@users.noreply.github.com>
2021-01-12 18:30:41 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,40 @@
+var speed, weight;
+var bullet, wall, thickness;
+
+function setup() {
+  createCanvas(1200,400);
+  speed = random(223,321);
+  weight = random(30,52);
+  thickness = random(22,83);
+
+  bullet = createSprite(50,200,20,30);
+  bullet.velocityX = speed;
+  bullet.shapeColor = color(255);
+  
+  wall = createSprite(1100,200,thickness,height/2);
+  wall.shapeColor = color(70,80,80);
+}
+
+function draw() {
+  background(0);  
+
+  if (hasCollided(bullet, wall)){
+    bullet.velocityX = 0;
+    var damage = 0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if (damage>10){
+      wall.shapeColor = color(255,0,0);
+    }
+    if (damage<10){
+      wall.shapeColor = color(0,255,0);
+    }
+  }
+  drawSprites();
+}
+function hasCollided(bullet1, wall1){
+  bulletRightEdge = bullet1.x+bullet1.width;
+  wallLeftEdge = wall1.x;
+  if (bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+    return false;
+}
